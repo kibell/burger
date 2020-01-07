9260e81ac6b6e11d8730d178e8f886c1b20eacde
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-06 19:38:38 -0600

    required express and mysql in the server file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
