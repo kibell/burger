5fd92e14104c68b766e7c3ef8e86a94fa4dd4912
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-06 20:24:29 -0600

    finished adding folders for the intiial setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+
+
+
+
+
+
+module.exports = router;
