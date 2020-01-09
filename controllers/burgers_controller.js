73a81d3b0a293a32d1d873dc2142369a6f01ef14
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-08 20:14:26 -0600

    required orm on the burger js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,19 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 
+router.get("/",function(req, res){
+    burger.all(function(data){
+let hbsObject = {
 
+    burgers: data 
+};
+console.log(hbsObject)
+res.render("index", hbsObject)
+
+    })
+
+
+})
 
 
 
